2a603f25c0fe0ecc8e8c8bc3c1220644e16bac63
Luca Palonca <lucapalonca@gmail.com>
2019-05-19 12:14:59 +0200

    extract more info for each reply

--- scrapers/reply.py
+++ scrapers/reply.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import logging
 import csv
+import datetime
 
 logging.basicConfig(format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', filename='replies.log', level=logging.INFO)
 
@@ -23,6 +24,43 @@ def get_reply(soup, user):
     return res
 
 
+def extract_info(soup, user):
+    replies = soup.find_all('div', {'class': 'content'})
+
+    res = []
+
+    for reply in replies:
+        name = reply.find('span', {'class' : 'FullNameGroup'}).get_text()
+        user = reply.find('span', {'class' : 'username'}).get_text()
+
+        replyTo = reply.find('div', {'class' : 'ReplyingToContextBelowAuthor'})
+        if replyTo is not None: 
+            replyTo = replyTo.get_text().strip()
+
+        timestamp = reply.find('span', {'class' : '_timestamp'})['data-time-ms']
+        date = datetime.datetime.fromtimestamp(int(timestamp)/1000)  
+
+        text = reply.find('p', {'class' : 'tweet-text'})
+        if text is not None: 
+            text = text.get_text().strip()
+
+        actions = reply.find_all('span', {'class' : 'ProfileTweet-actionCountForPresentation'})
+        
+
+        commentCount = 0
+        retweetCount = 0
+        favouriteCount = 0
+
+        if len(actions) > 0:
+            commentCount = actions[0].get_text()
+            retweetCount = actions[1].get_text()
+            favouriteCount = actions[3].get_text() 
+            
+        
+        res.append([name, user, replyTo, date, text, commentCount, retweetCount, favouriteCount])
+
+    return res
+
 
 
 def extract_replies(username, statusId):
@@ -36,7 +74,10 @@ def extract_replies(username, statusId):
     res = requests.get(baseURL)
     soup = BeautifulSoup(res.text, 'html.parser')
 
-    replies_list = get_reply(soup, username)
+    # replies_list = get_reply(soup, username)
+    replies_list = extract_info(soup, username)
+
+
     logging.info('Reply list ' + str(len(replies_list)))
 
     div = soup.find('div', {'class': 'stream-container'})
@@ -51,7 +92,8 @@ def extract_replies(username, statusId):
         min_pos = res['min_position']
 
         soup = BeautifulSoup(res['items_html'], 'html.parser')
-        replies_list += get_reply(soup, username)
+        # replies_list += get_reply(soup, username)
+        replies_list += extract_info(soup, username)
 
         logging.info('Reply list ' + str(len(replies_list)))
 
@@ -64,7 +106,7 @@ if __name__ == "__main__":
 
     print('Got {0} replies'.format(len(replies_list)))
 
-    filename = 'replies' + userId + '_' + statusId + '.csv'
+    filename = 'replies_' + userId + '_' + statusId + '.csv'
     with open('data/' + filename, 'w') as f:
         writer = csv.writer(f)
         writer.writerows(replies_list)
